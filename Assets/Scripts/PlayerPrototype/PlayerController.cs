1ab1117078498d91233e0be86fba983bf8dacdc1
ianco99 <71154684+ianco99@users.noreply.github.com>
2025-04-22 02:45:14 -0300

    Build

--- Assets/Scripts/PlayerPrototype/PlayerController.cs
+++ Assets/Scripts/PlayerPrototype/PlayerController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -52,6 +53,9 @@ public class PlayerController : MonoBehaviour
             canDash = false;
             dashing = true;
         }
+        
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+            SceneManager.LoadScene(0);
 
         if (dashing)
         {
