40aa902913bb7d22a904d346c50e5751b5f08b90
ianco99 <71154684+ianco99@users.noreply.github.com>
2025-04-22 01:49:02 -0300

    Implemented dash with cooldown

--- Assets/Scripts/PlayerPrototype/PlayerController.cs
+++ Assets/Scripts/PlayerPrototype/PlayerController.cs
@@ -11,14 +11,19 @@ public class PlayerController : MonoBehaviour
 
     private Vector3 direction;
     public float speed = 6f;
+    public float maxSpeed = 600f;
     public float dashSpeed = 6f;
+    public float dashTimer = 0.25f;
     public float jumpSpeed = 6f;
     
     private bool grounded = false;
     private bool dashing = false;
+    private bool canDash = true;
 
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
+
+    private float currentDashTimer = 0f;
 
     private void Awake()
     {
@@ -42,16 +47,31 @@ public class PlayerController : MonoBehaviour
             _rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
         {
-            _rb.AddForce(transform.forward * dashSpeed);
+            canDash = false;
+            dashing = true;
+        }
+
+        if (dashing)
+        {
+            currentDashTimer += Time.deltaTime;
+
+            if (currentDashTimer >= dashTimer)
+            {
+                currentDashTimer = 0;
+                dashing = false;
+                canDash = true;
+                
+                _rb.linearVelocity = Vector3.zero;
+            }
         }
     }
 
 
     private void FixedUpdate()
     {
-        if (direction.magnitude >= 0.01f)
+        if (direction.magnitude >= 0.01f && !dashing)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity,
@@ -61,12 +81,20 @@ public class PlayerController : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
             float prevY = _rb.linearVelocity.y;
+            
             _rb.linearVelocity = moveDir * (speed * Time.fixedDeltaTime);
             _rb.linearVelocity = new Vector3(_rb.linearVelocity.x, prevY, _rb.linearVelocity.z);
         }
-        else
+        
+        if (dashing)
         {
-            //_rb.linearVelocity = new Vector3(0, _rb.linearVelocity.y, 0f);
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity,
+                turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            _rb.linearVelocity = moveDir * dashSpeed * Time.fixedDeltaTime;
         }
     }
 
