17fbd7e1e0ddd8858f5dad15db4d926200825207
MateoMonastra <mateomonastravm@gmail.com>
2025-04-27 18:35:32 -0300

    Add PropulsorPlatform

---
+++ Assets/Scripts/Platforms/PropulsorPlatform.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace Platforms
+{
+    public class PropulsorPlatform : MonoBehaviour
+    {
+        public float jumpForce;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                Rigidbody playerRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+                if (playerRigidbody != null)
+                {
+                    playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                }
+            }
+        }
+    }
+}
