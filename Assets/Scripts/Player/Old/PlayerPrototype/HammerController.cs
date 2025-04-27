c8a7884a90b87b466d7ab3dd0007b99ba4fffa35
ianco99 <71154684+ianco99@users.noreply.github.com>
2025-04-27 03:07:44 -0300

    Hammer first implementation

---
+++ Assets/Scripts/Player/Old/PlayerPrototype/HammerController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class HammerController : MonoBehaviour
+{
+    private Animator animator;
+    private bool isAnimating = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Get the Animator component attached to this GameObject
+        animator = GetComponent<Animator>();
+        if (animator == null)
+        {
+            Debug.LogError("No Animator component found on this GameObject!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check for left mouse button click and if we're not currently animating
+        if (Input.GetMouseButtonDown(0) && !isAnimating)
+        {
+            PlayHammerAnimation();
+        }
+    }
+
+    void PlayHammerAnimation()
+    {
+        if (animator != null)
+        {
+            isAnimating = true;
+            animator.SetTrigger("Attack");
+        }
+    }
+
+    // This method should be called by an Animation Event at the end of your animation
+    public void OnAnimationComplete()
+    {
+        isAnimating = false;
+    }
+}
