b238298328cf7b6bfc57d335d9b6e7c2f1e1e37d
ianco99 <71154684+ianco99@users.noreply.github.com>
2025-05-19 17:32:26 -0300

    Fixed dash-propulsor bug

--- Assets/Scripts/Platforms/PropulsorPlatform.cs
+++ Assets/Scripts/Platforms/PropulsorPlatform.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using KinematicCharacterController;
+using KinematicCharacterController.Examples;
 using UnityEngine;
 
 namespace Platforms
@@ -6,19 +9,35 @@ namespace Platforms
     public class PropulsorPlatform : MonoBehaviour
     {
         public float jumpForce;
+        private Dictionary<Collider, bool> collisions = new Dictionary<Collider, bool>();
 
         private void OnTriggerEnter(Collider other)
         {
+            if (!collisions.ContainsKey(other))
+                collisions.Add(other, false);
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (collisions[other])
+                return;
+
+
             if (other.gameObject.CompareTag("Player"))
             {
                 var kinematicCharacterMotor = other.gameObject.GetComponent<KinematicCharacterMotor>();
+                var characterController = other.gameObject.GetComponent<ExampleCharacterController>();
 
                 var hammerController = other.gameObject.GetComponentInChildren<HammerController>();
-                
-                if (kinematicCharacterMotor != null)
+
+                if (kinematicCharacterMotor != null && characterController != null)
                 {
-                    kinematicCharacterMotor.ForceUnground();
-                    kinematicCharacterMotor.BaseVelocity = transform.up * jumpForce;
+                    if (characterController.CurrentCharacterState == CharacterState.Default)
+                    {
+                        collisions[other] = true;
+                        kinematicCharacterMotor.ForceUnground();
+                        kinematicCharacterMotor.BaseVelocity = transform.up * jumpForce;
+                    }
                 }
 
                 if (hammerController != null)
@@ -26,6 +45,12 @@ namespace Platforms
                     hammerController.InterruptGroundSlam();
                 }
             }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (collisions.ContainsKey(other))
+                collisions.Remove(other);
         }
     }
 }
