09caa8c7a3b7618a6407123ba3803d0e6381037d
MateoMonastra <mateomonastravm@gmail.com>
2025-06-08 21:17:42 -0300

    Update CandyPendulum.cs

--- Assets/Scripts/Hazards/CandyPendulum/CandyPendulum.cs
+++ Assets/Scripts/Hazards/CandyPendulum/CandyPendulum.cs
@@ -6,11 +6,18 @@ namespace Hazards.CandyPendulum
 {
     public class CandyPendulum : MonoBehaviour
     {
+        private enum SwingDirection
+        {
+            X,
+            Z
+        }
+
+        [SerializeField] private SwingDirection mode = SwingDirection.Z;
         [SerializeField] private float swingAngle = 45f;
         [SerializeField] private float swingSpeed = 1.5f;
         [SerializeField] private float knockbackForce = 20f;
         [SerializeField] private Transform pendulumArm;
-        // [SerializeField] private float knockbackForce;
+
 
         private float _time;
 
@@ -18,7 +25,15 @@ namespace Hazards.CandyPendulum
         {
             _time += Time.deltaTime;
             float angle = swingAngle * Mathf.Sin(_time * swingSpeed * Mathf.PI * 2);
-            pendulumArm.localRotation = Quaternion.Euler(0f, 0f, angle);
+
+            if (mode == SwingDirection.Z)
+            {
+                pendulumArm.localRotation = Quaternion.Euler(0f, 0f, angle);
+            }
+            else if (mode == SwingDirection.X)
+            {
+                pendulumArm.localRotation = Quaternion.Euler(angle, 0f, 0f);
+            }
         }
 
         private void OnTriggerEnter(Collider other)
@@ -27,7 +42,7 @@ namespace Hazards.CandyPendulum
             // if (!other.TryGetComponent(out ExampleCharacterController characterController)) return;
             // characterController.DeathSequence(transform.position);
 
-            // esto quedaría si seria solo empuje, pero decidí que sea muerte 
+            // esto quedaría si seria solo empuje
             if (!other.CompareTag("Player")) return;
             var characterController = other.GetComponent<ExampleCharacterController>();
             if (!characterController) return;
