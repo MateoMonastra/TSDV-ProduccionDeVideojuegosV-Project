1bdafdb3d6b64de876a170e4e902c9d4161277b4
ianco99 <71154684+ianco99@users.noreply.github.com>
2025-07-02 21:35:05 -0300

    Player attack only hits once

--- Assets/Scripts/Enemies/KillEnemy.cs
+++ Assets/Scripts/Enemies/KillEnemy.cs
@@ -6,23 +6,32 @@ using UnityEngine;
 public class KillEnemy : MonoBehaviour
 {
     [SerializeField] private HammerController hammerController;
-    private List<Collider> attacked = new();
-
+    private bool _dealingDamage = false;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Enemy"))
+        if (_dealingDamage)
         {
-            if (other.gameObject.TryGetComponent(out HealthController enemy))
+            if (other.CompareTag("Enemy"))
             {
-                enemy.Damage(1);
+                if (other.gameObject.TryGetComponent(out HealthController enemy))
+                {
+                    enemy.Damage(1);
+                    hammerController.ToggleAttackCollider(false);
+                    _dealingDamage = false;
+                }
+            }
+
+            //TODO: YA HAY QUE SACAR ESTE BODRIO
+            if (other.gameObject.TryGetComponent(out IBreakable breakable))
+            {
+                if (hammerController.IsGroundSlamming)
+                    breakable.Break();
             }
         }
+    }
 
-        //TODO: YA HAY QUE SACAR ESTE BODRIO
-        if (other.gameObject.TryGetComponent(out IBreakable breakable))
-        {
-            if (hammerController.IsGroundSlamming)
-                breakable.Break();
-        }
+    public void StartAttack(bool value)
+    {
+        _dealingDamage = value;
     }
 }
