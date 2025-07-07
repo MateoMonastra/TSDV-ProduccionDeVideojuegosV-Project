149fa99ea7970c9aa21d7517ac27fb6b0474b601
ianco99 <71154684+ianco99@users.noreply.github.com>
2025-07-07 00:37:20 -0300

    Breakable platform impl

--- Assets/Scripts/Platforms/BreakablePlatform.cs
+++ Assets/Scripts/Platforms/BreakablePlatform.cs
@@ -1,10 +1,13 @@
 using PickUps;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Platforms
 {
     public class BreakablePlatform : Pickup, IBreakable
     {
+        [SerializeField] private UnityEvent OnBreak;
+
         public void Break()
         {
             RefreshCooldown();
