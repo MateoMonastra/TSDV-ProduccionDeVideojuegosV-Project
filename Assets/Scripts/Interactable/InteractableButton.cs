28172224c63846c48695a36e17f6f7fd55ccdc9a
Ian Kuznicki <iankuz25@gmail.com>
2025-09-01 16:08:04 -0300

    Create interactable classes

---
+++ Assets/Scripts/Interactable/InteractableButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Interactable
+{
+    public class InteractableButton : IInteractable
+    {
+        [SerializeField] private UnityEvent onInteract;
+        private bool interacting;
+
+        public bool IsInteractable()
+        {
+            return interacting;
+        }
+
+        public void Interact()
+        {
+            onInteract?.Invoke();
+        }
+    }
+}
