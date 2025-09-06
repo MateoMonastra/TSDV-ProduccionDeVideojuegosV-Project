4e54e09d5a695d89afd0090b2e90a5768cd1716a
MateoMonastra <mateomonastravm@gmail.com>
2025-09-06 05:53:58 -0300

    Update KillBound.cs

--- Assets/Scripts/CheckPoint/KillBound.cs
+++ Assets/Scripts/CheckPoint/KillBound.cs
@@ -1,18 +1,35 @@
-using System;
-using Health;
-using KinematicCharacterController.Examples;
 using UnityEngine;
+using Health;
 
 namespace CheckPoint
 {
+    [RequireComponent(typeof(Collider))]
+    [RequireComponent(typeof(Rigidbody))]
     public class KillBound : MonoBehaviour
     {
-        [SerializeField] private (int, int) knockback = (20,5);
+        private Collider _col;
+        private Rigidbody _rb;
+
+        private void Awake()
+        {
+            _col = GetComponent<Collider>();
+            _col.isTrigger = true;              
+
+            _rb = GetComponent<Rigidbody>();    
+            _rb.isKinematic = true;           
+            _rb.useGravity  = false;
+        }
+
         private void OnTriggerEnter(Collider other)
         {
-            if (!other.gameObject.CompareTag("Player")) return;
-            if (!other.TryGetComponent(out HealthController controller)) return;
-            controller.InstaKill(new DamageInfo(0, transform.position,knockback));
+            var health = other.GetComponentInParent<HealthController>();
+            if (health == null)
+            {
+                Debug.LogWarning($"KillBound: no se encontró HealthController en la jerarquía de {other.name}", other);
+                return;
+            }
+
+            health.InstaKill(); // Esto debe disparar OnDeath en HealthController
         }
     }
 }
