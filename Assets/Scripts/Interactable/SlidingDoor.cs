de23d3b574489b70dcf8f133e4525673ca50f482
Ian Kuznicki <iankuz25@gmail.com>
2025-09-29 16:57:09 -0300

    Alternating behaviours implementation

--- Assets/Scripts/Interactable/SlidingDoor.cs
+++ Assets/Scripts/Interactable/SlidingDoor.cs
@@ -4,7 +4,8 @@ public class SlidingDoor : MonoBehaviour
 {
     [SerializeField] private float slidingTime;
     [SerializeField] private float yDistance;
-
+    [SerializeField] private bool startDownwards;
+    
     private Vector3 startingPos;
     private Vector3 endPos;
     private bool _isTimerRunning;
@@ -17,9 +18,21 @@ public class SlidingDoor : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+
+        if (startDownwards)
+        {
+            currentSign = 1f;
+            _currentTimer = slidingTime;
         startingPos = transform.position;
-        endPos = transform.position - Vector3.up * yDistance;
-        _currentTimer = slidingTime;
+        endPos = transform.position + Vector3.up * yDistance;
+        }
+        else
+        {
+        startingPos = transform.position + Vector3.up * yDistance;
+        endPos = transform.position;
+            currentSign = -1f;
+            _currentTimer = 0;
+        }
     }
 
 
@@ -50,13 +63,13 @@ public class SlidingDoor : MonoBehaviour
         _isTimerRunning = true;
         currentSign *= -1f;
 
-        // if (currentSign > 0)
-        // {
-        //     _currentTimer = 0;
-        // }
-        // else
-        // {
-        //     _currentTimer = slidingTime;
-        // }
+        if (_currentTimer < 0)
+        {
+            _currentTimer = 0;
+        }
+        else if(_currentTimer > slidingTime)
+        {
+            _currentTimer = slidingTime;
+        }
     }
 }
