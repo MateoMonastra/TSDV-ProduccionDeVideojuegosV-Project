fb3528f688a46b15a7ad659b1b2d29a412189a42
ianco99 <71154684+ianco99@users.noreply.github.com>
2025-10-20 05:34:15 -0300

    Attack polish

--- Assets/Scripts/Enemies/BaseEnemy/States/Attack.cs
+++ Assets/Scripts/Enemies/BaseEnemy/States/Attack.cs
@@ -49,10 +49,6 @@ namespace Enemies.BaseEnemy.States
                 _onAttackHit?.Invoke();
                 _collider.gameObject.SetActive(true);
             }
-            else
-            {
-                enemy.LookAt(player);
-            }
 
             if (!_delayed) return;
 
@@ -70,6 +66,8 @@ namespace Enemies.BaseEnemy.States
 
         public override void Exit()
         {
+            enemy.LookAt(player, Vector3.up);
+
             base.Exit();
         }
     }
