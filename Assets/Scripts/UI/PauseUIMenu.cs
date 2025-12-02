c392a715a30066ba879fc74522e28b1a681ac942
ianco99 <71154684+ianco99@users.noreply.github.com>
2025-12-02 13:17:37 -0300

    Pause menu renamed

--- Assets/Scripts/UI/PauseUIMenu.cs
+++ Assets/Scripts/UI/PauseUIMenu.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PauseMenu : MonoBehaviour {
+public class PauseUIMenu : MonoBehaviour {
 
     public static bool GameIsPaused = false;
     
