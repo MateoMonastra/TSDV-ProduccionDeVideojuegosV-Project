4b1cba0cc17ea212b7bfcaedbe3cb11db921c5b7
ianco99 <71154684+ianco99@users.noreply.github.com>
2026-02-23 12:58:48 -0300

    Implement enemy get up anim

--- Assets/Scripts/Enemies/BaseEnemy/States/SpinningVerticalImpulse.cs
+++ Assets/Scripts/Enemies/BaseEnemy/States/SpinningVerticalImpulse.cs
@@ -10,18 +10,24 @@ namespace Enemies.BaseEnemy.States
         private TrailRenderer _trailRenderer;
         private Action _onImpulseStarted;
         private Action _onImpulseEnded;
+        private Action _onGetUp;
         private Vector3 _impulseSource;
         private readonly RaycastHit[] _hit = new RaycastHit[1];
         private Vector2 _impulseForce;
         private float elapsed = 0;
-        
-        public SpinningVerticalImpulse(Transform enemy, Transform player, TrailRenderer trailRenderer, BaseEnemyModel model, UnityEngine.AI.NavMeshAgent agent,
-            Rigidbody rigidbody, Action onImpulseStarted, Action onImpulseEnded) : base(enemy, player, model)
+        private float recoveryElapsed = 0;
+        private bool _isGrounded;
+
+        public SpinningVerticalImpulse(Transform enemy, Transform player, TrailRenderer trailRenderer,
+            BaseEnemyModel model, UnityEngine.AI.NavMeshAgent agent,
+            Rigidbody rigidbody, Action onImpulseStarted, Action onImpulseEnded, Action onGetUp) : base(enemy, player,
+            model)
         {
             _agent = agent;
             _rigidbody = rigidbody;
             _onImpulseStarted = onImpulseStarted;
             _onImpulseEnded = onImpulseEnded;
+            _onGetUp = onGetUp;
             _trailRenderer = trailRenderer;
         }
 
@@ -39,7 +45,9 @@ namespace Enemies.BaseEnemy.States
             enemy.LookAt(toPlayer);
 
             elapsed = 0;
-            
+            recoveryElapsed = 0;
+            _isGrounded = false;
+
             _trailRenderer.enabled = true;
 
             _rigidbody.AddForce(
@@ -51,31 +59,62 @@ namespace Enemies.BaseEnemy.States
         {
             base.Tick(delta);
 
-            elapsed += delta;
-            if (_rigidbody.linearVelocity.y < 0)
-            {
-                _rigidbody.linearVelocity += Vector3.up * (Physics.gravity.y * (model.SpinningFallMultiplier - 1) * delta);
-            }
-            else if (_rigidbody.linearVelocity.y > 0)
-            {
-                _rigidbody.linearVelocity += Vector3.up * (Physics.gravity.y * (model.SpinningLowJumpMultiplier - 1) * delta);
-            }
-
-            Quaternion deltaRotation = Quaternion.Euler(-460 * delta, 0, 0);
-        
-            _rigidbody.MoveRotation(_rigidbody.rotation * deltaRotation);
             
-            GroundCheck();
+
+            if (!_isGrounded)
+            {
+                elapsed += delta;
+                if (_rigidbody.linearVelocity.y < 0)
+                {
+                    _rigidbody.linearVelocity +=
+                        Vector3.up * (Physics.gravity.y * (model.SpinningFallMultiplier - 1) * delta);
+                }
+                else if (_rigidbody.linearVelocity.y > 0)
+                {
+                    _rigidbody.linearVelocity +=
+                        Vector3.up * (Physics.gravity.y * (model.SpinningLowJumpMultiplier - 1) * delta);
+                }
+
+                Quaternion deltaRotation = Quaternion.Euler(-460 * delta, 0, 0);
+
+                _rigidbody.MoveRotation(_rigidbody.rotation * deltaRotation);
+                
+                GroundCheck();
+            }
+            else
+            {
+                recoveryElapsed += delta;
+                if (recoveryElapsed > 1.6f)
+                {
+                    _onGetUp?.Invoke();
+                }
+            }
         }
 
         private void GroundCheck()
         {
-            Debug.DrawRay(enemy.position + enemy.up * 1.2f,Vector3.down * 2.2f, Color.yellow);
-            bool isGrounded = Physics.Raycast(enemy.position + enemy.up * 1.2f, Vector3.down, 2.2f, model.GroundLayer);
+            Ray rayo = new Ray(enemy.position + enemy.up * 1.6f, Vector3.down * 1.4f);
+            Debug.DrawRay(rayo.origin, rayo.direction, Color.yellow);
+            bool isGrounded = Physics.Raycast(rayo, out RaycastHit hit,1.4f,model.GroundLayer);
 
-            if (isGrounded && elapsed > 0.65f)  //Enough time to get off the ground
+            if (isGrounded && elapsed > 0.65f) //Enough time to get off the ground
             {
                 _onImpulseEnded?.Invoke();
+                
+                //_agent.enabled = true;
+                enemy.position = hit.point;
+                _rigidbody.isKinematic = true;
+                _trailRenderer.enabled = false;
+
+
+                _rigidbody.rotation = Quaternion.identity;
+                //_agent.ResetPath();
+                Vector3 toPlayer = _impulseSource;
+                toPlayer.y = enemy.position.y;
+                enemy.LookAt(toPlayer);
+                
+
+                _isGrounded = true;
             }
         }
 
@@ -90,7 +129,7 @@ namespace Enemies.BaseEnemy.States
             _rigidbody.isKinematic = true;
             _trailRenderer.enabled = false;
 
-            
+
             _rigidbody.rotation = Quaternion.identity;
             _agent.ResetPath();
         }
