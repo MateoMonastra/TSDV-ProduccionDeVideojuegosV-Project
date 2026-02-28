1dca55da80586512e7dbdacfa532b6851b01fcf1
Julian Tinao <d.juliant@gmail.com>
2026-02-28 00:23:41 -0300

    Lever and DoorSlide sound working

--- Assets/Scripts/Audio/DoorSlideAudio.cs
+++ Assets/Scripts/Audio/DoorSlideAudio.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class DoorSlideAudio : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private AK.Wwise.Event _doorSlide;
+    [SerializeField]
+    private SlidingDoor _slidingDoor; 
+    
+    void OnEnable()
     {
-        
+        _slidingDoor.SubePorton.AddListener(DoorSlide);
     }
-
-    // Update is called once per frame
-    void Update()
+    void OnDisable()
     {
-        
+        _slidingDoor.SubePorton.RemoveListener(DoorSlide);
     }
+    
+    void DoorSlide()
+    {
+        _doorSlide.Post(this.gameObject);
+        Debug.Log("AUDIO: DoorSlide");
+    }
+    
 }
