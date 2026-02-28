b9b2fbbb354c6d36eedb1d11c06f2371758301cf
ianco99 <71154684+ianco99@users.noreply.github.com>
2026-02-28 17:37:30 -0300

    Implement sliding door delay

--- Assets/Scripts/Interactable/SlidingDoor.cs
+++ Assets/Scripts/Interactable/SlidingDoor.cs
@@ -4,12 +4,14 @@ using UnityEngine.Events;
 public class SlidingDoor : MonoBehaviour
 {
     public UnityEvent SubePorton;
+    [SerializeField] private float preWarmtime;
     [SerializeField] private float slidingTime;
     [SerializeField] private float yDistance;
 
     private Vector3 startingPos;
     private Vector3 endPos;
     private bool _isTimerRunning;
+    private bool _isWarming;
 
     private float currentSign = 1f;
     private float _currentTimer;
@@ -19,7 +21,7 @@ public class SlidingDoor : MonoBehaviour
     void Start()
     {
         currentSign = 1f;
-        _currentTimer = slidingTime;
+        _currentTimer = 0;
         startingPos = transform.position;
         endPos = transform.position + Vector3.up * yDistance;
     }
@@ -27,38 +29,54 @@ public class SlidingDoor : MonoBehaviour
 
     void Update()
     {
-        if (_isTimerRunning)
+        if (_isWarming)
         {
-            _currentTimer += Time.deltaTime * currentSign;
+            _currentTimer += Time.deltaTime;
 
-            float t = _currentTimer / slidingTime;
-            Vector3 nowPos = Vector3.Lerp(endPos, startingPos, t);
-            transform.position = nowPos;
-
-            if (_currentTimer / slidingTime >= 1)
+            if (_currentTimer > preWarmtime)
             {
-                _isTimerRunning = false;
+                currentSign *= -1f;
+
+                if (currentSign > 0)
+                {
+                    _currentTimer = 0;
+                }
+                else if (currentSign < 0)
+                {
+                    _currentTimer = slidingTime;
+                }
+                
+                _isWarming = false;
+                SubePorton?.Invoke();
             }
-            else if (_currentTimer / slidingTime <= 0)
+        }
+        else
+        {
+            if (_isTimerRunning)
             {
-                _isTimerRunning = false;
+                _currentTimer += Time.deltaTime * currentSign;
+
+                float t = _currentTimer / slidingTime;
+                Vector3 nowPos = Vector3.Lerp(endPos, startingPos, t);
+                transform.position = nowPos;
+
+                if (_currentTimer / slidingTime >= 1)
+                {
+                    _isTimerRunning = false;
+                }
+                else if (_currentTimer / slidingTime <= 0)
+                {
+                    _isTimerRunning = false;
+                }
             }
         }
     }
 
     public void Slide()
     {
-        SubePorton?.Invoke();
         _isTimerRunning = true;
-        currentSign *= -1f;
+        _isWarming = true;
 
-        if (_currentTimer < 0)
-        {
-            _currentTimer = 0;
-        }
-        else if (_currentTimer > slidingTime)
-        {
-            _currentTimer = slidingTime;
-        }
+        _currentTimer = 0;
     }
 }
