198dece8517033a22b872fc8fca15251f74bf165
ianco99 <71154684+ianco99@users.noreply.github.com>
2026-03-03 06:28:26 -0300

    Main menu escape feedback

--- Assets/Scripts/LevelManager/MainMenu.cs
+++ Assets/Scripts/LevelManager/MainMenu.cs
@@ -6,21 +6,29 @@ public class MainMenu : MonoBehaviour
 {
     private static readonly int Trigger = Animator.StringToHash("Trigger");
     [SerializeField] private Animator animator;
+    [SerializeField] private Animator escAnimator;
 
     public void TriggerFeedback()
     {
         animator.SetTrigger(Trigger);
         StartCoroutine(ChangeScene());
     }
-    
+
     public void TriggerExit()
     {
-       Application.Quit();
+        escAnimator.SetTrigger(Trigger);
+        StartCoroutine(QuitApp());
     }
-
+    
     private IEnumerator ChangeScene()
     {
         yield return new WaitForSeconds(0.9f);
         SceneManager.LoadScene("Lvl_1_Castillo");
+    }
+    
+    private IEnumerator QuitApp()
+    {
+        yield return new WaitForSeconds(0.9f);
+        Application.Quit();
     }
 }
